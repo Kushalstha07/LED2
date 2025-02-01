f5fa8dcd8a7090ee0cb513c7f26b92c6adb6c0d4
Amrit Bhattarai <amritbhattarai481@gmail.com>
2025-02-01 12:47:49 +0545

    Login and Registration Form created

---
+++ src/ledproject2/LedProject2.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ledproject2;
+
+import ledproject2.view.LoginForm;
+
+/**
+ *
+ * @author Nitro 5
+ */
+public class LedProject2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        LoginForm login = new LoginForm();
+        login.setVisible(true);
+        
+    }
+    
+}
